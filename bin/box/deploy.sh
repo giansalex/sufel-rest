dd98b918a5a18400363a392afbd100fcc5d7572b
Giancarlos Salas <giansalex@gmail.com>
2018-04-21 13:43:59 -0500

    fix deploy repo

--- bin/box/deploy.sh
+++ bin/box/deploy.sh
@@ -3,6 +3,6 @@
 ZIP_ASSET=sufel.phar.zip
 php zip.php ./$ZIP_ASSET ./dist
 USER=giansalex
-REPO=sufel
+REPO=sufel-rest
 GH_ASSET="https://uploads.github.com/repos/$USER/$REPO/releases/$TRAVIS_TAG/assets?name=$ZIP_ASSET"
 curl --data-binary @"./$ZIP_ASSET" -H "Authorization: token $GITHUB_TOKEN" -H "Content-Type: application/zip" $GH_ASSET
