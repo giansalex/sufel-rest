72f2080c0608665df6bc0417fab0f4d544726e2d
Giancarlos Salas <giansalex@gmail.com>
2018-04-22 21:28:00 -0500

    update zip files routine

--- bin/box/zip.php
+++ bin/box/zip.php
@@ -2,12 +2,29 @@
 
 function zip($zipPath, $folder) {
     $zip = new ZipArchive();
-    if (!$zip->open($zipPath, ZIPARCHIVE::OVERWRITE))
+    if (!$zip->open($zipPath, ZipArchive::CREATE | ZIPARCHIVE::OVERWRITE))
         die("Failed to create archive\n");
 
-    $zip->addGlob("$folder/*");
-    if (!$zip->status == ZIPARCHIVE::ER_OK)
-        echo "Failed to write files to zip\n";
+    /** @var SplFileInfo[] $files */
+    $files = new RecursiveIteratorIterator(
+        new RecursiveDirectoryIterator($folder),
+        RecursiveIteratorIterator::LEAVES_ONLY
+    );
+
+    foreach ($files as $name => $file) {
+        // Skip directories (they would be added automatically)
+        if ($file->isDir()) {
+            continue;
+        }
+        // Get real and relative path for current file
+        $filePath = $file->getRealPath();
+        $relativePath = substr($filePath, strlen($folder) + 1);
+
+        // Add current file to archive
+        $zip->addFile($filePath, $relativePath);
+        if (!$zip->status == ZIPARCHIVE::ER_OK)
+            echo "Failed to write files to zip\n";
+    }
 
     $zip->close();
 }
